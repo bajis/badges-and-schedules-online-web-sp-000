8dc8f28007c9a437ec5d46d781c4c221701063be
Kelsey Bajis <bajis.simple.haven@gmail.com>
2019-04-21 07:23:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,10 +3,16 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
+def batch_badge_creator(guest)
+  array = []
+  guest.each do |name|
+    array.push ("Hello, my name is #{name}.")
+  end
+  return array
+end
+
 def assign_rooms
 
 end
 
-def batch_badge_creator(names)
 
-end
